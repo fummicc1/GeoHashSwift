0cca7ee55b5688fae982140ba2d56dbaecdd8b56
fummicc1 <fumiya.tennis1@gmail.com>
2024-11-24 18:13:25 +0900

    Fix bit order bug on GeoHashCoordinate2D

--- Sources/GeoHashFramework/GeoHashCoordinate2D.swift
+++ Sources/GeoHashFramework/GeoHashCoordinate2D.swift
@@ -26,31 +26,28 @@ public struct GeoHashCoordinate2D: Sendable, Hashable {
         let longitudeBits = binary.enumerated().filter { $0.offset % 2 == 0 }.map { String($0.element) }
         
         // 1111 -> 15
-        let latitudePrecision = pow(2, Double(latitudeBits.count))
-        let latitudeDelta = 180.0 / latitudePrecision
+        let latitudePrecision = pow(2, Double(latitudeBits.count)) - 1
         // 01000 -> 8/15
         let latitudeIndex = latitudeBits.enumerated().reduce(into: 0.0, { partialResult, value in
             let (index, bits) = value
             if bits == "1" {
-                partialResult += Double(1 << index)
+                partialResult += Double(1 << (latitudeBits.count - 1 - index))
             }
         }) / latitudePrecision
-        self.latitude = latitudeDelta * latitudeIndex - 90.0
-        
-        // 00000 -> 32
-        let longitudePrecision = pow(2, Double(longitudeBits.count))
-        let longitudeDelta = 360.0 / longitudePrecision
-        // 01000 -> 8/15
+        self.latitude = 180 * latitudeIndex - 90.0
+
+        // 00000 -> 31
+        let longitudePrecision = pow(2, Double(longitudeBits.count)) - 1
+        // 01000 -> 8/31
         let longitudeIndex = Double(
             longitudeBits.enumerated().reduce(into: 0.0, { partialResult, value in
                 let (index, bits) = value
                 if bits == "1" {
-                    partialResult += Double(1 << index)
-                    
+                    partialResult += Double(1 << (longitudeBits.count - 1 - index))
                 }
             })
         ) / longitudePrecision
         
-        self.longitude = longitudeDelta * longitudeIndex - 180.0
+        self.longitude = 360 * longitudeIndex - 180.0
     }
 }
--- Tests/GeoHashFrameworkTests/GeoHashTests.swift
+++ Tests/GeoHashFrameworkTests/GeoHashTests.swift
@@ -55,27 +55,34 @@ struct GeoHashTests {
 
     @Test
     func getBound() async throws {
-        let precision = GeoHashBitsPrecision.exact(digits: 0)
-        let actual = GeoHash(
-            binary: String(repeating: "0", count: precision.rawValue),
+        // Tokyo Station
+        let coordinate = GeoHashCoordinate2D(
+            latitude: 35.681382,
+            longitude: 139.766084
+        )
+        let precision = GeoHashBitsPrecision.low
+        let geoHash = GeoHash(
+            latitude: coordinate.latitude,
+            longitude: coordinate.longitude,
             precision: precision
-        ).getBound()
+        )
+        let actual = geoHash.getBound()
         let expected = [
             GeoHashCoordinate2D(
-                latitude: 90.0,
-                longitude: -180.0
+                latitude: 35.687429258303666,
+                longitude: 139.7668385876034
             ),
             GeoHashCoordinate2D(
-                latitude: 90.0,
-                longitude: 180.0
+                latitude: 35.687429258303666,
+                longitude: 139.7778249157284
             ),
             GeoHashCoordinate2D(
-                latitude: -90.0,
-                longitude: 180.0
+                latitude: 35.681936094241166,
+                longitude: 139.7778249157284
             ),
             GeoHashCoordinate2D(
-                latitude: -90.0,
-                longitude: -180.0
+                latitude: 35.681936094241166,
+                longitude: 139.7668385876034
             ),
         ]
         #expect(actual == expected)
@@ -104,7 +111,7 @@ struct GeoHashTests {
             "xn76urwd",
             "xn76urw6",
             "xn76urw7",
-            "xn76urwk"
+            "xn76urwk",
         ]
 
         let geoHash = GeoHash(latitude: lat, longitude: lng)
