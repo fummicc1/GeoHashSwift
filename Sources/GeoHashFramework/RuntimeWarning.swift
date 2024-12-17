a5f4918dfc07083a93c14d31b8c3952a7458bf7a
fummicc1 <fumiya.tennis1@gmail.com>
2024-12-18 00:09:14 +0900

    Use dsohandle macro

--- Sources/GeoHashFramework/RuntimeWarning.swift
+++ Sources/GeoHashFramework/RuntimeWarning.swift
@@ -8,31 +8,12 @@
 import Foundation
 import OSLog
 
-actor RuntimeWarning {
-    var info = Dl_info()
-
-    init() {
-#if DEBUG
-        dladdr(
-            dlsym(
-                dlopen(
-                    nil,
-                    RTLD_LAZY
-                ),
-                """
-                $sxSg7SwiftUI8CommandsA2bCRzlAbCP4body4BodyQzvgTW
-                """
-            ),
-            &info
-        )
-        #endif
-    }
-
-    func log(message: StaticString, args: any CVarArg...) {
+enum RuntimeWarning {
+    static func log(message: StaticString, args: any CVarArg...) {
         #if DEBUG
         os_log(
             .fault,
-            dso: info.dli_fbase,
+            dso: #dsohandle,
             log: OSLog(
                 subsystem: "com.apple.runtime-issues",
                 category: "ReportRuntimeWarningSample"
