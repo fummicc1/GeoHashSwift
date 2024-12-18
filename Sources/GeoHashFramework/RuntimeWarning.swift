6a5a0e168574741e071f756e703f5362db6ff031
fummicc1 <fumiya.tennis1@gmail.com>
2024-12-18 09:34:29 +0900

    Use same manner as one that pointfree does

--- Sources/GeoHashFramework/RuntimeWarning.swift
+++ Sources/GeoHashFramework/RuntimeWarning.swift
@@ -11,9 +11,26 @@ import OSLog
 enum RuntimeWarning {
     static func log(message: StaticString, args: any CVarArg...) {
         #if DEBUG
+        var dso: UnsafeRawPointer?
+        // ref: https://github.com/pointfreeco/swift-issue-reporting/blob/a3f634d1a409c7979cabc0a71b3f26ffa9fc8af1/Sources/IssueReporting/IssueReporters/RuntimeWarningReporter.swift#L39-L55
+        let count = _dyld_image_count()
+        for i in 0..<count {
+          if let name = _dyld_get_image_name(i) {
+            let swiftString = String(cString: name)
+            if swiftString.hasSuffix("/SwiftUI") {
+              if let header = _dyld_get_image_header(i) {
+                dso = UnsafeRawPointer(header)
+              }
+            }
+          }
+        }
+        guard let dso = dso else {
+            assertionFailure("Failed to get DSO")
+            return
+        }
         os_log(
             .fault,
-            dso: #dsohandle,
+            dso: dso,
             log: OSLog(
                 subsystem: "com.apple.runtime-issues",
                 category: "ReportRuntimeWarningSample"
